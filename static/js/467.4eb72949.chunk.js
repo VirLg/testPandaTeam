108be8379b9da6706be17fab3cd6b25c3e49a005
VirLg <108211942+VirLg@users.noreply.github.com>
2023-08-07 19:09:58 +0000

    Deploying to gh-pages from @ VirLg/testPandaTeam@a4c0eaaa32cb181997938b787037eada2401ebe3 🚀

---
+++ static/js/467.4eb72949.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[467],{467:function(e,t,r){r.r(t),r.d(t,{default:function(){return o}});var a=r(184);function o(){return(0,a.jsxs)("mesh",{rotation:[-Math.PI/2,0,0],position:[0,-3.4,0],receiveShadow:!0,groundColor:"black",children:[(0,a.jsx)("planeGeometry",{args:[20,20]}),(0,a.jsx)("meshPhongMaterial",{color:"white",attach:"material"})]})}}}]);
+//# sourceMappingURL=467.4eb72949.chunk.js.map
